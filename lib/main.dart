0f9a94bb0fee12008fd97278c1ba9c14a828072d
harshithva <harshith11032001@gmail.com>
2020-07-06 20:47:32 +0530

    List created

--- lib/main.dart
+++ lib/main.dart
@@ -9,17 +9,53 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: Scaffold(
-        appBar: AppBar(title: Text('Brand Generator')),
-        body: Center(child: Text(wordPair.asPascalCase)),
-      ),
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.red,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: RandomWords());
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  final _randomWordPairs = <WordPair>[];
+
+  Widget _buildList() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(8),
+        itemBuilder: (BuildContext context, int index) {
+          if (index.isOdd) return Divider();
+
+          final i = index ~/ 2;
+
+          if (i >= _randomWordPairs.length) {
+            _randomWordPairs.addAll(generateWordPairs().take(10));
+          }
+
+          return _buildRow(_randomWordPairs[index]);
+        });
+  }
+
+  Widget _buildRow(pair) {
+    return ListTile(
+      title: (Text(
+        pair.asPascalCase,
+        style: TextStyle(fontSize: 18.0),
+      )),
+    );
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Brand Generator')),
+      body: _buildList(),
     );
   }
 }
